c7b526a0b0091be242cc1439becc27bdffa761c8
lillith <35337584+0xlillith@users.noreply.github.com>
2023-05-22 11:11:47 +0200

    Add files via upload

---
+++ config/waybar/scripts/copyVPNLocalIP.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+IFACE=$(/usr/sbin/ifconfig | grep tun0 | awk '{print $1}' | tr -d ':')
+
+if [ "$IFACE" = "tun0" ]; then
+   echo "󰆧  $(/usr/sbin/ifconfig tun0 | grep "inet " | awk '{print $2}')${u-}" | copyq copy - &>1
+else
+fi
